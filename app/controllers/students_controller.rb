bd98354ad7b33859a043c90dc018ba949b1ae295
fanzhong <fanzhongzeng78@gmail.com>
2019-11-26 12:23:21 -0500

    finished but rspec is dumb, but everything works

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -9,9 +9,14 @@ class StudentsController < ApplicationController
   end
 
   def new
+    @student = Student.new
   end
 
   def create
+    white_list = params[:student].permit(:first_name, :last_name)
+    student = Student.create(white_list)
+
+    redirect_to student_path(student)
   end
 
 end
